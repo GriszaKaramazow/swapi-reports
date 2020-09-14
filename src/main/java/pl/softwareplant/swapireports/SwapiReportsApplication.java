c3ca8598ab38f9435f3d8849fb7b5d8c23d12323
Grzegorz Witkowski <grisza.karamazow@gmail.com>
2020-09-14 15:08:52 +0200

    Adds Spring Boot

---
+++ src/main/java/pl/softwareplant/swapireports/SwapiReportsApplication.java
@@ -0,0 +1,13 @@
+package pl.softwareplant.swapireports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwapiReportsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwapiReportsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/softwareplant/swapireports/SwapiReportsApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.softwareplant.swapireports;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwapiReportsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
